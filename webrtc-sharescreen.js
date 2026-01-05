52358c55c8c836fec1e7a354ba96d333c0b296f9
Danny Ruijters <danny.ruijters@philips.com>
2026-01-05 22:48:48 +0100

    fix share overlay

--- webrtc-sharescreen.js
+++ webrtc-sharescreen.js
@@ -4,17 +4,6 @@
 
 // Share screen overlay window functions
 function openShareOverlay() {
-    const roomId = document.getElementById('roomId').value;
-    const userName = document.getElementById('userName').value;
-    
-    // Build share link with current URL and parameters
-    const url = new URL(window.location.href);
-    url.searchParams.set('room', roomId);
-    if (userName) {
-        url.searchParams.set('name', userName);
-    }
-    
-    document.getElementById('shareLink').value = url.toString();
     document.getElementById('shareOverlay').classList.add('active');
 }
 
